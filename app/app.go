6be852873bc6828b53e657a28bdbb8fea6d30e87
Antonio Lopes <antoniolopesg@protonmail.com>
2023-07-07 16:12:44 -0300

    add server name in the internet functionality

--- app/app.go
+++ app/app.go
@@ -13,17 +13,26 @@ func Make() *cli.App {
 	app := cli.NewApp()
 	app.Name = "Net Tools"
 	app.Usage = "Search IP Addresses and Server names in the Internet"
+
+	flags := []cli.Flag{
+		cli.StringFlag{
+			Name:  "host",
+			Value: "github.com",
+		},
+	}
+
 	app.Commands = []cli.Command{
 		{
-			Name:  "ip",
-			Usage: "Find the IP Address of given a host",
-			Flags: []cli.Flag{
-				cli.StringFlag{
-					Name:  "host",
-					Value: "github.com",
-				},
-			},
+			Name:   "ip",
+			Usage:  "Find the IP Address of given a host",
+			Flags:  flags,
 			Action: findIps,
+		},
+		{
+			Name:   "servers",
+			Usage:  "Find the server name in the internet",
+			Flags:  flags,
+			Action: findServers,
 		},
 	}
 	return app
@@ -40,5 +49,19 @@ func findIps(c *cli.Context) {
 
 	for _, ip := range ips {
 		fmt.Println(ip)
+	}
+}
+
+func findServers(c *cli.Context) {
+	host := c.String("host")
+
+	servers, err := net.LookupNS(host)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, server := range servers {
+		fmt.Println(server.Host)
 	}
 }
