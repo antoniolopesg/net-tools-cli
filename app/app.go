5d4e3a700ffc37b497480b4b6ed5d51a21a84256
Antonio Lopes <antoniolopesg@protonmail.com>
2023-07-07 15:25:30 -0300

    add first command to lookup ip from a given host

--- app/app.go
+++ app/app.go
@@ -1,6 +1,10 @@
 package app
 
 import (
+	"fmt"
+	"log"
+	"net"
+
 	"github.com/urfave/cli"
 )
 
@@ -9,5 +13,32 @@ func Make() *cli.App {
 	app := cli.NewApp()
 	app.Name = "Net Tools"
 	app.Usage = "Search IP Addresses and Server names in the Internet"
+	app.Commands = []cli.Command{
+		{
+			Name:  "ip",
+			Usage: "Find the IP Address of given a host",
+			Flags: []cli.Flag{
+				cli.StringFlag{
+					Name:  "host",
+					Value: "github.com",
+				},
+			},
+			Action: findIps,
+		},
+	}
 	return app
+}
+
+func findIps(c *cli.Context) {
+	host := c.String("host")
+
+	ips, err := net.LookupIP(host)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, ip := range ips {
+		fmt.Println(ip)
+	}
 }
